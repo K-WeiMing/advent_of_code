2a203fab5eef7640bc1345076fd4a73cd35123dd
wei_ming_kng <weiming.kng@gmail.com>
2023-01-07 00:37:17 +0800

    first commit

---
+++ 6/day_six.py
@@ -0,0 +1,37 @@
+import os
+
+
+def part_one(data):
+    # Time complexity: O(n)
+    # Space complexity: O(1)
+
+    # Use a sliding window technique to find the answer
+    for i in range(len(data) - 3):
+
+        if len(set(data[i : i + 4])) == 4:
+            return i + 4
+
+
+def part_two(data):
+    # Time complexity: O(n)
+    # Space complexity: O(1)
+
+    # Use a sliding window technique to find the answer
+    for i in range(len(data) - 13):
+
+        if len(set(data[i : i + 14])) == 14:
+            return i + 14
+
+
+if __name__ == "__main__":
+
+    # Read in the data
+    curr_path = os.path.dirname(os.path.abspath(__file__))
+    input_path = "./input.txt"
+    with open(os.path.join(curr_path, input_path), "r") as file:
+        data = file.readlines()
+
+    data = data[0]
+
+    print(f"Answer for part 1: {part_one(data)}")
+    print(f"Answer for part 2: {part_two(data)}")
