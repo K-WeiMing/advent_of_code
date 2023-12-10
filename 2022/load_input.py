5fac04e5ce7c5358d7348797a584a2173582ceb9
wei_ming_kng <weiming.kng@gmail.com>
2023-12-10 23:35:02 +0800

    day one solution

--- 2022/load_input.py
+++ 2022/load_input.py
@@ -3,14 +3,15 @@ Helper function to read in the data based on the current folder structure
 """
 import os
 
+
 def load_data():
     f_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     input_file_name = "inputs.txt"
-    f_path = os.path.join(f_path,"inputs" ,input_file_name)
+    f_path = os.path.join(f_path, "inputs", input_file_name)
     print(f_path)
     with open(f_path, "r") as file:
         data = file.readlines()
-        
+
     # Replace '\n' in the inputs
     data = [str(i.replace("\n", "")) for i in data]
     return data
--- 2023/01.py
+++ 2023/01.py
@@ -1,5 +1,60 @@
+from typing import List
 from load_input import load_data
 
-data = load_data()
+data: List[str] = load_data()
+
+mapping = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
 
 
+def part_one():
+    calibration_values = 0
+
+    for d in data:
+        nums = []
+        l, r = 0, len(d) - 1
+        while l < len(d):
+            if d[l].isnumeric():
+                nums.append(d[l])
+                break
+            l += 1
+
+        while r >= 0:
+            if d[r].isnumeric():
+                nums.append(d[r])
+                break
+            r -= 1
+        calibration_values += int("".join(nums))
+
+    return calibration_values
+
+
+def part_two():
+    calibration_values = 0
+
+    for d in data:
+        nums = []
+        for index, char in enumerate(d):
+            if char.isdigit():
+                nums.append(char)
+                continue
+
+            for key, value in mapping.items():
+                if d[index:].startswith(key):
+                    nums.append(str(value))
+                    break
+        calibration_values += int(nums[0] + nums[-1])
+    return calibration_values
+
+
+print(part_one())
+print(part_two())
